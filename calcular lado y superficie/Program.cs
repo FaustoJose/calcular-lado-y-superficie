68ec9a76290669d0ffb2c123029b495dba49bfd9
Fausto Jose Tineo Mejia <2016-2155@est.itsc.edu.do>
2021-08-08 20:52:45 -0400

    Agregar archivos de proyecto.

---
+++ calcular lado y superficie/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace calcular_lado_y_superficie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuadrado(5);
+        }
+        public static void Cuadrado(int lado)
+        {
+            int perimetro=4*lado;
+            int superficie= lado*lado ;
+            Console.WriteLine("el perimetro es: "+perimetro+" la superficie es: "+superficie);
+        }
+    }
+}
